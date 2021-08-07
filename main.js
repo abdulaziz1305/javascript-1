cf1fa73fbac5e5ebe5c894fabd4b473429abf798
abdulaziz <abdulaziz@mail.ru>
2021-08-07 23:31:31 +0500

    js-1

---
+++ main.js
@@ -0,0 +1,78 @@
+// console.log(5 + 5)
+// var num = 5;
+// console.log(num)
+//var a = 5000;
+//var b = 6000;
+//var d = 7000;
+//var amount = 5
+//console.log((a + b + d)*amount)
+//var num = 5;
+//console.log(++num)
+// promt()
+//var user__name = prompt('ismingizni kiriting');
+//var user__born = prompt('yilingiz kiriting');
+//console.log('Salom ' + user__name + '.' + ' sizni yoshingiz ' +(2021 - user__born));
+//CONDITION
+//var user_info = +prompt('5 + 5 = ?');
+
+//if(user_info == (5 + 5)){
+    //console.log('topdiz');
+//}else if(user_info ==11){
+    //console.log('koproq');
+//}else if(user_info == 9){
+    //console.log('kamroq');
+//}else{
+    //console.log('topomadiz')
+//}
+
+//var num1 = 25;
+//var num2 = 15;
+ //switch (num1 + num2){
+ //   case 40:
+ //       console.log('birinchi');
+//        break;
+ //       case 23:
+ //       console.log('ikkinchi');
+ //       break;
+ //       case 5:
+ //       console.log('uchinchi');
+ //       break;
+ //      case 40:
+ //       console.log('tortinchi');
+ //       break;
+  //      case 85:
+  //      console.log('beshinchi');
+  //      break;
+   //     default:
+  //          console.log("hammasi noto'g'ri");
+    //        break;
+//}
+//var user__log = prompt("nima bor")
+//if('pul' == user__log || 'bilim' == user__log){
+   // console.log('kirdiz')
+//}else{
+    //console.log('kirmadiz')
+//}
+//var login = 'abdulaziz';
+/////var pass ='2005';
+//var user__login = prompt('loginni kiriting');
+//var user__pass = prompt('parolni kiriting');
+///if(user__login == login && user__pass == pass){
+    //alert('kirdiz')
+   //var whoYouAre = confirm("akkaunt o'ziznikimi?");
+   //if(whoYouAre){
+       //alert('Hush kelibsiz');
+   //}else{
+      // alert('akkaunt buzilgan')    
+    //}
+//}else{
+   // alert('kirmadiz');
+///}
+prompt
+var user__name =  prompt('Ismingizni kiriting');
+if(user__name == 'Abdulaziz'){
+    alert('Hello Boss');
+}
+else{
+    alert('Hello Guest');
+}
